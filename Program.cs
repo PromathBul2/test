784cec33658fa0f7f10802d111897d1a265bebd9
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-11-17 14:31:55 +0300

    Tуst

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 3; i >= 0; i--)
+{
+    sum += num / (int)Math.Pow(10.0, i);
+    num = num % (int)Math.Pow(10.0, i);
+}
+Console.WriteLine($"Сумма цифр введенного числа = {sum}");
